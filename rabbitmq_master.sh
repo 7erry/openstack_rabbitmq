c7b785e4eb0d2711ea45f5d8b2a4fe07df8c2fc5
7erry <twal7ers@gmail.com>
2012-07-20 10:38:39 -0400

    commited a riak file...

--- rabbitmq_master.sh
+++ rabbitmq_master.sh
@@ -5,26 +5,50 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get upgrade -y
 
-apt-get install -y wget
+# update sources to include rabbit's latest releases
+cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
+deb http://www.rabbitmq.com/debian/ testing main
+EOF
 
-# install as per Basho
-wget http://downloads.basho.com/riak/CURRENT/riak_1.1.4-1_amd64.deb
-apt-get install -y libssl0.9.8
-dpkg -i riak_1.1.4-1_amd64.deb
+curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc -o /tmp/rabbitmq-signing-key-public.asc
+apt-key add /tmp/rabbitmq-signing-key-public.asc
+rm /tmp/rabbitmq-signing-key-public.asc
 
-# get our riak config files from cloud.nimbus
-sudo wget "http://cloud.nimbus.att.net/app.config"
-sudo wget "http://cloud.nimbus.att.net/vm.args"
-# change the ip address to be that of eth0
-echo "/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print \$1}' " >~/myip.sh
-chmod o+x ~/myip.sh
-echo 'perl -p -i -e s/127.0.0.1/$1/g *' >~/rip.sh
-chmod o+x ~/rip.sh
-~/rip.sh `~/myip.sh`
-cat app.config > /etc/riak/app.config
-cat vm.args > /etc/riak/vm.args
+apt-get -qy update
+apt-get -qy install rabbitmq-server
 
-# start it up
-riak start
+# shh we are hunting wabbits
+sleep 5
+pkill -u rabbitmq
+
+# it is important that rabbits eat lettuce 
+echo 'rabbitseatlettuce' > /var/lib/rabbitmq/.erlang.cookie
+chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
+chmod 400 /var/lib/rabbitmq/.erlang.cookie
+
+# add plugins
+wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/cowboy-0.5.0-rmq2.8.2-git4b93c2d.ez 
+wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/sockjs-0.2.1-rmq2.8.2-gitfa1db96.ez 
+wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/rabbitmq_web_stomp-2.8.2.ez 
+wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/rabbitmq_web_stomp_examples-2.8.2.ez
+
+cp *.ez /usr/lib/rabbitmq/lib/rabbitmq_server-2.8.4/plugins
+
+#enable plugins
+#rabbitmq-plugins enable rabbitmq_shovel
+#rabbitmq-plugins enable rabbitmq_management
+#rabbitmq-plugins enable rabbitmq_stomp
+#rabbitmq-plugins enable rabbitmq_web_stomp
+#rabbitmq-plugins enable rabbitmq_web_stomp_examples
+# or we could just create the plugins file
+echo '[rabbitmq_shovel,rabbitmq_management,rabbitmq_stomp,rabbitmq_web_stomp,rabbitmq_web_stomp_examples].' > /etc/rabbitmq/enabled_plugins
+
+# if we have EBS we should symlink the message store
+#/var/lib/rabbitmq/
+#/var/logs/rabbitmq/
+
+# either way we have to restart rabbit
+#/etc/init.d/rabbitmq-server start
+/usr/sbin/rabbitmq-server -detached
 
 # done
