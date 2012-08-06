ce221f90c05c0e427891746ad43087fd33f0bf7a
7erry <twal7ers@gmail.com>
2012-08-06 14:55:55 -0400

    Updated scripts to refer to 2.8.? for the directory... Most likely should replace the line to a completely blind copy /*/...

--- rabbitmq_node.sh
+++ rabbitmq_node.sh
@@ -5,6 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get upgrade -y
 
+# add snmp daemon
+apt-get install -y snmpd
+#sed –i 's/-V systemonly//g' /etc/snmp/snmpd.conf
+#sed –i 's/^agentAddress/#agentAddress/g' /etc/snmp/snmpd.conf
+#sed -i 's/^#agentAddress udp:161/agentAddress udp:161/g' /etc/snmp/snmpd.conf
+#/etc/init.d/snmpd restart
+
 # update sources to include rabbit's latest releases
 cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
 deb http://www.rabbitmq.com/debian/ testing main
@@ -32,7 +39,7 @@ wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/sockjs-0.
 wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/rabbitmq_web_stomp-2.8.2.ez 
 wget http://www.rabbitmq.com/releases/plugins/v2.8.2-web-stomp-preview/rabbitmq_web_stomp_examples-2.8.2.ez
 
-cp *.ez /usr/lib/rabbitmq/lib/rabbitmq_server-2.8.4/plugins
+cp *.ez /usr/lib/rabbitmq/lib/rabbitmq_server-2.8.?/plugins
 
 #enable plugins
 #rabbitmq-plugins enable rabbitmq_shovel
